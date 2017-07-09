8955d64dce9dbc5da33dc0262c4d438efed45bd8
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2017-07-09 14:35:27 +0800

    add auto browser open on start and translate remaining pages with language state

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Styles from '../styles/styles';
 
-const Contact = () => (
+const Contact = (props) => (
   <div style={Styles.contentLayer}>
-    <div>Ping me an Email at <a href="mailto:domdunnett@gmai.com">domdunnett@gmail.com</a></div>
-    <div>Connect with me on <a href="https://www.linkedin.com/in/domdunnett">LinkedIn</a></div>
+    {
+      props.language === 'Eng' ?
+      <div>
+        <div>Ping me an Email at <a href="mailto:domdunnett@gmai.com">domdunnett@gmail.com</a></div>
+        <div>Connect with me on <a href="https://www.linkedin.com/in/domdunnett">LinkedIn</a></div>
+      </div>
+      :
+      <div>
+        <div>发一封邮件在<a href="mailto:domdunnett@gmai.com">domdunnett@gmail.com</a></div>
+        <div>连接在<a href="https://www.linkedin.com/in/domdunnett">LinkedIn</a>上</div>
+      </div>
+    }
   </div>
 );
 
--- app/components/Tech.jsx
+++ app/components/Tech.jsx
@@ -2,40 +2,79 @@ import React from 'react';
 import Styles from '../styles/styles';
 import SkyscannerLogo from './SkyscannerLogo';
 
-const Tech = () => (
+const Tech = props => (
   <div style={Styles.contentLayer}>
-    <p>
-      Currently I am a full stack developer @
-      <a href="https://www.skyscanner.com">
-        <SkyscannerLogo alt="Skyscanner" />
-      </a>
-    </p>
-    <p>I am comfortable working in all areas of web development from initial design,
-      to both frontend and backend development.</p>
-    <p>Tech Experience:</p>
-    <ul>
-      <li>Javascript</li>
-      <ul>
-        <li>React.js (this site is built using React)</li>
-        <li>AngularJS</li>
-        <li>Nodejs</li>
-        <li>Meteorjs</li>
-      </ul>
-      <li>Web Design</li>
-      <ul>
-        <li>Sketch</li>
-        <li>Adobe Illustrator</li>
-      </ul>
-      <li>Python</li>
-      <ul>
-        <li>Django</li>
-        <li>Flask</li>
-      </ul>
-      <li>Ruby</li>
-      <ul>
-        <li>Rails</li>
-      </ul>
-    </ul>
+    {
+      props.language === 'Eng' ?
+        <div>
+          <p>
+          Currently I am a full stack developer @
+          <a href="https://www.skyscanner.com">
+            <SkyscannerLogo alt="Skyscanner" />
+          </a>
+          </p>
+          <p>I am comfortable working in all areas of web development from initial design,
+          to both frontend and backend development.</p>
+          <p>Tech Experience:</p>
+          <ul>
+            <li>Javascript</li>
+            <ul>
+              <li>React.js (this site is built using React)</li>
+              <li>AngularJS</li>
+              <li>Nodejs</li>
+              <li>Meteorjs</li>
+            </ul>
+            <li>Web Design</li>
+            <ul>
+              <li>Sketch</li>
+              <li>Adobe Illustrator</li>
+            </ul>
+            <li>Python</li>
+            <ul>
+              <li>Django</li>
+              <li>Flask</li>
+            </ul>
+            <li>Ruby</li>
+            <ul>
+              <li>Rails</li>
+            </ul>
+          </ul>
+        </div>
+      :
+        <div>
+          <p>
+          我在
+          <a href="https://www.skyscanner.com">
+            <SkyscannerLogo alt="Skyscanner" />
+          </a>
+          的软件工程师工作
+          </p>
+          <p>我的经历是在</p>
+          <ul>
+            <li>Javascript</li>
+            <ul>
+              <li>React.js (这个网站用React.js)</li>
+              <li>AngularJS</li>
+              <li>Nodejs</li>
+              <li>Meteorjs</li>
+            </ul>
+            <li>Web Design</li>
+            <ul>
+              <li>Sketch</li>
+              <li>Adobe Illustrator</li>
+            </ul>
+            <li>Python</li>
+            <ul>
+              <li>Django</li>
+              <li>Flask</li>
+            </ul>
+            <li>Ruby</li>
+            <ul>
+              <li>Rails</li>
+            </ul>
+          </ul>
+        </div>
+    }
   </div>
 );
 
